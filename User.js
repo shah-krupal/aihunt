10374775e40cb909bf3f2967721eccfdf652f18a
shah-krupal <77288288+shah-krupal@users.noreply.github.com>
2023-11-12 22:14:25 +0530

    removed bcrypt

--- User.js
+++ User.js
@@ -1,47 +1,45 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('./config/database.js');
-const bcrypt = require('bcrypt-nodejs');
+const bcryptjs = require('bcryptjs'); // Use bcryptjs instead of bcrypt
 
 const User = sequelize.define('user', {
-    email:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey:true,
-        unique: true
-    },
-    password:{
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    role:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    googleId:{
-        type: DataTypes.STRING,
-        allowNull: true,
-    }
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  }
 });
 
 User.beforeCreate(async (user) => {
-    if(user.password)
-    {
-        try {
-            const saltRounds = 10; 
-            const hashedPassword = await bcrypt.hash(user.password, saltRounds);
-            user.password = hashedPassword;
-          } catch (error) {
-            throw new Error('Error hashing the password: ' + error.message);
-          }
+  if (user.password) {
+    try {
+      const saltRounds = 10;
+      const hashedPassword = await bcryptjs.hash(user.password, saltRounds); // Use bcryptjs.hash
+      user.password = hashedPassword;
+    } catch (error) {
+      throw new Error('Error hashing the password: ' + error.message);
     }
-  });
+  }
+});
 
-  const syncDatabase = async () => {
-    await User.sync();
-  };
-  
-  // Call the function to synchronize the database
-  syncDatabase();
-  
+const syncDatabase = async () => {
+  await User.sync();
+};
+
+// Call the function to synchronize the database
+syncDatabase();
 
 module.exports = User;
