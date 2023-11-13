950c0eaf67e41d7d96590367ce331bdbef9e6b51
shah-krupal <77288288+shah-krupal@users.noreply.github.com>
2023-11-13 11:59:44 +0530

    tried my own session store

--- config/database.js
+++ config/database.js
@@ -7,26 +7,26 @@ const session = require('express-session');
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    logging:console.log
-  }
-);
-// const sequelize = new Sequelize('postgres://default:gQA5wxdkf6mz@ep-noisy-brook-58368266.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb',{
-//   dialect: 'postgres',
-//   dialectModule: pg,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//     },
-//   },
-//   logging:console.log,
-// })
+// const sequelize = new Sequelize(
+//   dbConfig.database,
+//   dbConfig.username,
+//   dbConfig.password,
+//   {
+//     host: dbConfig.host,
+//     dialect: dbConfig.dialect,
+//     logging:console.log
+//   }
+// );
+const sequelize = new Sequelize('postgres://default:gQA5wxdkf6mz@ep-noisy-brook-58368266.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb',{
+  dialect: 'postgres',
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+  logging:console.log,
+})
 
 
 
